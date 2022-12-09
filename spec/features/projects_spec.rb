cf474027b48e96b72baf4299b26cd97a2a56fbaa
root <pwoods@uccs.edu>
2022-12-09 04:47:09 +0000

    Various fixes

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.feature "Projects", type: :feature do
   context "Create new project" do
+
     before (:each) do
-      sign_in user
       visit new_project_path
       within("form") do
         fill_in "Title", with: "Test title"
@@ -24,8 +24,8 @@ RSpec.feature "Projects", type: :feature do
 
   context "Update project" do
     let(:project) { Project.create(title: "Test title", description: "Test content") }
+
     before(:each) do
-      sign_in user
       visit edit_project_path(project)
     end
 
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,7 +6,6 @@ require File.expand_path('../config/environment', __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 
-require 'devise'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -35,9 +34,6 @@ end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-  config.include Devise::Test::ControllerHelpers, type: :controller
-  config.include Devise::Test::IntegrationHelpers, type: :request
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
